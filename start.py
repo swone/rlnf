fdffe2a868718acc725ec3d179590eb75cfd67bd
Shawn Im <shawnim8@gmail.com>
2018-07-05 02:25:42 -0400

    ford-fulkerson working

--- start.py
+++ start.py
@@ -14,29 +14,36 @@ def generate_random_graph(numV, p, max_flow):
 
 def depth_first_search(graph, source, sink, discovered, max_flow):
     discovered.append(source)
-    current = source
     if source == sink:
         return max_flow, discovered
-    for all i in range(current, len(graph[0])):
-        if graph[current][i] > 0:
-            if graph[current][i] not in discovered:
-                current = i
-                found, discovered = depth_first_search(graph, current, sink, discovered, max_flow)
-                return min(found, graph[source][current]), discovered
-    return 0, discovered
+    for i in range(len(graph[0])):
+        if graph[source][i] > 0:
+            if i not in discovered:
+                found, discovered = depth_first_search(graph, i, sink, discovered, max_flow)
+                if found != -1:
+                    return min(found, graph[source][i]), discovered
+    discovered.pop()
+    return -1, discovered
 
-def ford_fulkerson(graph, source, sink):
-    discovered = []
+def ford_fulkerson(graph, source, sink, max_flow):
     update = graph
     flow = 0
     maxed = False
     while not maxed:
-        add, path = depth_first_search(graph, source, sink, discovered, max_flow)
+        discovered = []
+        add, path = depth_first_search(update, source, sink, discovered, max_flow)
         if add > 0:
             flow += add
             for i in range(len(path)-1):
                 update[path[i]][path[i+1]] -= add
-                update[path[i]][path[i+1]] += add
+                update[path[i+1]][path[i]] += add
         else:
             maxed = True
     return flow       
+
+ex_graph = [[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0]]
+source = 1
+sink = 3
+max_flow = 10
+
+print(ford_fulkerson(ex_graph, source, sink, 10))
